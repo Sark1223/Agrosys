e34b865190a6fd5a7238f7a7cb4a6ade6e244fe1
Karla Judith Santos Rivera <105814864+Sark1223@users.noreply.github.com>
2026-05-18 22:26:49 -0600

    Migraciones

--- agrosys-task/src/main/resources/db/migration/V4__recreate_special_tasks.sql
+++ agrosys-task/src/main/resources/db/migration/V4__recreate_special_tasks.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS agrosys_task.WORKER_TASK;
 DROP TABLE IF EXISTS agrosys_task.SPECIAL_TASK;
 
-CREATE TABLE agrosys_task.SPECIAL_TASK (
+CREATE TABLE IF NOT EXISTS agrosys_task.SPECIAL_TASK (
     special_task_id  INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     payment_amount DECIMAL(18,2) NOT NULL,
@@ -12,13 +12,4 @@ CREATE TABLE agrosys_task.SPECIAL_TASK (
     PRIMARY KEY (special_task_id)
 ) ENGINE= InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE agrosys_task.WORKER_TASK(
-    worker_task_id INT NOT NULL AUTO_INCREMENT,
-    worker_id INT NOT NULL,
-    special_task_id INT NOT NULL,
-    PRIMARY KEY (worker_task_id),
-    UNIQUE KEY uk_worker_special (worker_id, special_task_id),
-    FOREIGN KEY (worker_id) REFERENCES agrosys_worker.WORKER(workerId) ON DELETE CASCADE,
-    FOREIGN KEY (special_task_id) REFERENCES agrosys_task.SPECIAL_TASK(special_task_id) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
     
