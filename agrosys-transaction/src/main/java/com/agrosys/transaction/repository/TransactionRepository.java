fb19732a6b9994d8efa4ae20a558f991e6e49e1c
samuelVega29 <samuel.vegacat@gmail.com>
2026-05-19 18:43:35 -0600

    Update TransactionRepository.java

--- agrosys-transaction/src/main/java/com/agrosys/transaction/repository/TransactionRepository.java
+++ agrosys-transaction/src/main/java/com/agrosys/transaction/repository/TransactionRepository.java
@@ -1,5 +1,26 @@
 package com.agrosys.transaction.repository;
 
-public class TransactionRepository {
-    
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.agrosys.transaction.entity.AgrosysTransaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<AgrosysTransaction, Integer> {
+
+    @Query(value = """
+    SELECT * FROM agrosys_db.transactions t 
+
+    WHERE t.create_at BETWEEN : startDate AND
+
+    :endDate AND(
+
+    :type IS NULL OR t.transaction_type = :type) ORDER BY
+                t.create_at DESC, t.transaction_id DESC""", nativeQuery = true)
+    List<AgrosysTransaction> findByFilters(LocalDate startDate, LocalDate endDate, String type);
+
 }
