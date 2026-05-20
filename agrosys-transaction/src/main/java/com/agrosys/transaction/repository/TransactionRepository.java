77fcba57c17b0da10e745c4864f16f869e19558b
samuelVega29 <samuel.vegacat@gmail.com>
2026-05-20 17:31:02 -0600

    Servicio implementado para transactions
    Métodos CRUD

--- agrosys-transaction/src/main/java/com/agrosys/transaction/repository/TransactionRepository.java
+++ agrosys-transaction/src/main/java/com/agrosys/transaction/repository/TransactionRepository.java
@@ -13,14 +13,15 @@ import com.agrosys.transaction.entity.AgrosysTransaction;
 public interface TransactionRepository extends JpaRepository<AgrosysTransaction, Integer> {
 
     @Query(value = """
-    SELECT * FROM agrosys_db.transactions t 
-
-    WHERE t.create_at BETWEEN : startDate AND
-
-    :endDate AND(
-
-    :type IS NULL OR t.transaction_type = :type) ORDER BY
-                t.create_at DESC, t.transaction_id DESC""", nativeQuery = true)
+        SELECT * FROM agrosys_db.
+    transactions t 
+        WHERE t.create_at 
+    BETWEEN :startDate AND :endDate 
+            AND(:type IS NULL OR 
+    t.transaction_type = :type)
+        ORDER BY t.create_at 
+    DESC, t.transaction_id DESC
+        """, nativeQuery = true)
     List<AgrosysTransaction> findByFilters(LocalDate startDate, LocalDate endDate, String type);
 
 }
--- agrosys-transaction/src/main/java/com/agrosys/transaction/service/TransactionService.java
+++ agrosys-transaction/src/main/java/com/agrosys/transaction/service/TransactionService.java
@@ -1,5 +1,83 @@
 package com.agrosys.transaction.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.agrosys.transaction.dto.Response;
+import com.agrosys.transaction.dto.TransactionRequest;
+import com.agrosys.transaction.entity.AgrosysTransaction;
+import com.agrosys.transaction.entity.AgrosysTransactionType;
+import com.agrosys.transaction.repository.TransactionRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class TransactionService {
-    
+
+    private final TransactionRepository transactionRepository;
+
+    public List<AgrosysTransaction> getAll(
+            LocalDate startDate, LocalDate endDate, String type
+    ) {
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("Las fechas de inicio y fin son obligatorias");
+        }
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("La fecha de incio no puede ser matoy a la fecha de fin");
+        }
+        log.info("Obteniendo transacciones desde {} hasta {} tipo {}", startDate, endDate, type);
+        return transactionRepository.findByFilters(startDate, endDate, type);
+    }
+
+    public AgrosysTransaction getById(Integer id) {
+    return transactionRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Transacción no encontrada"));
+    }
+
+    @Transactional
+    public Response create(TransactionRequest request) {
+        AgrosysTransaction transaction = new AgrosysTransaction();
+        transaction.setTransactionType(AgrosysTransactionType.valueOf(request.getTransactionType()));
+        transaction.setCreateAt(request.getCreateAt());
+        transaction.setAmount(request.getAmount());
+        transaction.setDescription(request.getDescription());
+        transaction.setLotTradeId(request.getLotTradeId());
+        transactionRepository.save(transaction);
+        log.info("Transaccion creada: {}", transaction);
+
+        return Response.builder().success(true).message("Transaccion registrada exitosamente").build();
+    }
+
+    @Transactional
+    public Response update(Integer id, TransactionRequest request) {
+        AgrosysTransaction transaction = getById(id);
+        transaction.setTransactionType(AgrosysTransactionType.valueOf(request.getTransactionType()));
+        transaction.setCreateAt(request.getCreateAt());
+        transaction.setAmount(request.getAmount());
+        transaction.setDescription(request.getDescription());
+        transaction.setLotTradeId(request.getLotTradeId());
+        transactionRepository.save(transaction);
+        log.info("Transacción actualizada: {}", transaction);
+        return Response.builder()
+                .success(true)
+                .message("Transacción actualizada exitosamente")
+                .build();
+    }
+
+    @Transactional
+    public Response delete(Integer id) {
+        AgrosysTransaction transaction = getById(id);
+        transactionRepository.delete(transaction);
+        log.info("Transacción eliminada: {}", id);
+        return Response.builder()
+                .success(true)
+                .message("Transacción eliminada exitosamente")
+                .build();
+    }
 }
